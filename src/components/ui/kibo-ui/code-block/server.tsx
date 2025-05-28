ae07e12c007bcf0b59e4afc15aaedaf094b32d9f
Richard Amare <amarerichard@icloud.com>
2025-05-28 09:30:18 +0200

    feat: add more components

---
+++ src/components/ui/kibo-ui/code-block/server.tsx
@@ -0,0 +1,60 @@
+import {
+  transformerNotationDiff,
+  transformerNotationErrorLevel,
+  transformerNotationFocus,
+  transformerNotationHighlight,
+  transformerNotationWordHighlight,
+} from '@shikijs/transformers'
+import { codeToHtml } from 'shiki'
+import type { BundledLanguage, CodeOptionsMultipleThemes } from 'shiki'
+import type { HTMLAttributes } from 'react'
+
+export type CodeBlockContentProps = HTMLAttributes<HTMLDivElement> & {
+  themes?: CodeOptionsMultipleThemes['themes']
+  language?: BundledLanguage
+  children: string
+  syntaxHighlighting?: boolean
+}
+
+export const CodeBlockContent = async ({
+  children,
+  themes,
+  language,
+  syntaxHighlighting = true,
+  ...props
+}: CodeBlockContentProps) => {
+  const html = syntaxHighlighting
+    ? await codeToHtml(children as string, {
+        lang: language ?? 'typescript',
+        themes: themes ?? {
+          light: 'vitesse-light',
+          dark: 'vitesse-dark',
+        },
+        transformers: [
+          transformerNotationDiff({
+            matchAlgorithm: 'v3',
+          }),
+          transformerNotationHighlight({
+            matchAlgorithm: 'v3',
+          }),
+          transformerNotationWordHighlight({
+            matchAlgorithm: 'v3',
+          }),
+          transformerNotationFocus({
+            matchAlgorithm: 'v3',
+          }),
+          transformerNotationErrorLevel({
+            matchAlgorithm: 'v3',
+          }),
+        ],
+      })
+    : children
+
+  return (
+    <div
+      // biome-ignore lint/security/noDangerouslySetInnerHtml: "Kinda how Shiki works"
+      dangerouslySetInnerHTML={{ __html: html }}
+      {...props}
+    />
+  )
+}
