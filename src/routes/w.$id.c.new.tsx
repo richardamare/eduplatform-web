b3f92e113e0bab111f48631e27c661633a6ec8df
Richard Amare <amarerichard@icloud.com>
2025-06-02 18:38:59 +0200

    chore: remove unncesessary

--- src/routes/w.$id.c.new.tsx
+++ src/routes/w.$id.c.new.tsx
@@ -1,9 +1,18 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { ChatId } from '@/types/chat'
 
 export const Route = createFileRoute('/w/$id/c/new')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/w/$id/c/new"!</div>
+  const { id } = Route.useParams()
+  const chatId = ChatId.make(id)
+
+  return (
+    <div>
+      <h1>New Chat</h1>
+      <p>Chat ID: {chatId.toString()}</p>
+    </div>
+  )
 }
